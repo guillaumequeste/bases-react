c896cea9cf347bbed5096dbea4b30c5091d8bdd3
Guillaume Queste <guillaume.queste@laposte.net>
2019-11-18 14:26:18 +0100

    formulaire emailjs complet avec message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,27 +4,48 @@ import Header from './Header'
 class Contact extends Component {
     constructor(props) {
         super(props);
-        this.state = { feedback: '', name: 'Gui', email: 'email@example.com' };
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { nom: '', prenom: '', email: '', telephone: '', message: '' };
+        this.handleChangeNom = this.handleChangeNom.bind(this);
+        this.handleChangePrenom = this.handleChangePrenom.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangeTelephone = this.handleChangeTelephone.bind(this);
+        this.handleChangeMessage = this.handleChangeMessage.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({feedback: event.target.value})
-      }
+    handleChangeNom(event) {
+        this.setState({nom: event.target.value})
+    }
+
+    handleChangePrenom(event) {
+        this.setState({prenom: event.target.value})
+    }
+
+    handleChangeEmail(event) {
+        this.setState({email: event.target.value})
+    }
+
+    handleChangeTelephone(event) {
+        this.setState({telephone: event.target.value})
+    }
+
+    handleChangeMessage(event) {
+        this.setState({message: event.target.value})
+    }
     
-      handleSubmit (event) {
+    handleSubmit (event) {
         const templateId = 'template_id'; // mettre son template_id
     
-        this.sendFeedback(templateId, {message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email})
-      }
+        this.sendFeedback(templateId, {nom: this.state.nom, prenom: this.state.prenom, email: this.state.email, telephone: this.state.telephone, message: this.state.message, })
+    }
     
       sendFeedback (templateId, variables) {
         window.emailjs.send(
           'gmail', templateId,
           variables
           ).then(res => {
-            document.querySelector("#message").innerHTML = 'message envoyé'
+            document.querySelector("#message").innerHTML = 
+                '<div style="font-weight: bold; text-align:center;"><h3>Message envoyé !</h3></div>'
           })
           // Handle errors here however you like, or use a React error boundary
           .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
@@ -34,20 +55,38 @@ class Contact extends Component {
         return (
             <div>
                 <Header />
-                <form className="test-mailing">
-                    <h1>Let's see if it works</h1>
+                <form>
+                    <h1>Envoyer un message</h1>
                     <div>
-                    <textarea
-                        id="test-mailing"
-                        name="test-mailing"
-                        onChange={this.handleChange}
-                        placeholder="Post some lorem ipsum here"
-                        required
-                        value={this.state.feedback}
-                        style={{width: '100%', height: '150px'}}
-                    />
+                        <input type="text"
+                                onChange={this.handleChangeNom}
+                                placeholder="Nom"
+                                required
+                                value={this.state.nom}/> 
+                        <input type="text"
+                                onChange={this.handleChangePrenom}
+                                placeholder="Prénom"
+                                required
+                                value={this.state.prenom}/>
+                            <input type="text"
+                                onChange={this.handleChangeEmail}
+                                placeholder="Email"
+                                required
+                                value={this.state.email}/>
+                            <input type="text"
+                                onChange={this.handleChangeTelephone}
+                                placeholder="Numéro de téléphone"
+                                required
+                                value={this.state.telephone}/>  
+                        <textarea
+                            onChange={this.handleChangeMessage}
+                            placeholder="Votre message"
+                            required
+                            value={this.state.message}
+                            style={{width: '100%', height: '150px'}}
+                        />
                     </div>
-                    <input type="button" value="Submit" className="btn btn--submit" onClick={this.handleSubmit} />
+                    <input type="button" value="Submit" onClick={this.handleSubmit} />
                 </form>
                 <div id="message"></div>
             </div>
