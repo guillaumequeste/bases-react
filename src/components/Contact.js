0584d50c615183ceeeb0019606b4818fe5a9d76b
Guillaume Queste <guillaume.queste@laposte.net>
2020-06-22 19:19:07 +0200

    emails update 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -218,7 +218,7 @@ class Contact extends Component {
                             id="message"
                             name="message"
                             onChange={this.handleChangeMessage}
-                            placeholder="Votre message"
+                            placeholder="Laisser votre message"
                             required
                             value={this.state.message}
                             rows="4"
