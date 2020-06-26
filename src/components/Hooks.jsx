e7f2c78fc1ce03f1b5c4e27fedce66e8412ed763
Guillaume Queste <guillaume.queste@laposte.net>
2020-06-26 15:13:00 +0200

    26 juin 2020

--- src/components/Hooks.jsx
+++ src/components/Hooks.jsx
@@ -1,22 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Header from './Header'
+import { watchFile } from 'fs';
 
 function Hooks() {
   // Déclare une nouvelle variable d'état, que l'on va appeler « count »
   const [count, setCount] = useState(0);
+  const [fruit, setFruit] = useState('banane');
 
+  // Équivalent à componentDidMount plus componentDidUpdate :
   useEffect(() => {
     // Met à jour le titre du document via l’API du navigateur
     document.title = `Vous avez cliqué ${count} fois`;
   });
 
+  
+
   return (
     <div>
-        <Header />
-      <p>Vous avez cliqué {count} fois</p>
-      <button onClick={() => setCount(count + 1)}>
-        {count}
-      </button>
+      <Header />
+      <div className="container">
+        <div>
+          <h2>useState</h2>
+          <p>Vous avez cliqué {count} fois</p>
+          <button onClick={() => setCount(count + 1)}>
+          {count}
+          </button>
+        </div>
+
+        <div>
+          <h2>useEffect</h2>
+          <p>Le fruit est {fruit}</p>
+          <button onClick={() => setFruit('orange')}>
+          {fruit}
+          </button>
+        </div>
+
+        
+      </div>
     </div>
   );
 }
